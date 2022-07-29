d0388466e94ee020a77e18c6819520281dee7439
rogerrm95 <rogerrm@live.com>
2022-07-29 12:21:11 -0300

    typescript: sobrescrevendo tipagem (Themes)

---
+++ src/@types/styles.d.ts
@@ -0,0 +1,12 @@
+import 'styled-components'
+import { defaultTheme } from '../styles/themes/default'
+
+export type ThemeType = typeof defaultTheme
+
+declare module 'styled-components' {
+    export interface DefaultTheme extends ThemeType {}
+}
+
+// Obs: styles.d.ts //
+// O 'd' significa que este arquivo irá apenas ter definições de tipos do Typescript e não código javascript em si //
+// Arquvio de definição de tipos //
