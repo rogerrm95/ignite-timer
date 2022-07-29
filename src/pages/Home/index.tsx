66eb6f5b4e57e4ac35c9ed9a120104de9e51c20a
rogerrm95 <rogerrm@live.com>
2022-07-29 20:35:53 -0300

    add: Estilização - Aprimorando os inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,13 +15,26 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TasksInput id="task" placeholder="Dê um nome para seu projeto" />
+          <TasksInput
+            id="task"
+            placeholder="Dê um nome para seu projeto"
+            list="task-suggestions"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+          </datalist>
 
           <label htmlFor="">durante</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutos.</span>
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -109,6 +109,10 @@ const BaseInput = styled.input`
 
 export const TasksInput = styled(BaseInput)`
   flex: 1;
+
+  &::-webkit-calendar-picker-indicator {
+    display: none !important;
+  }
 `
 
 export const MinutesAmountInput = styled(BaseInput)`
